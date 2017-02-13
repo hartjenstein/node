534fb498a6f555b94883db6b5d5fc0f00628862c
Nick Hartjenstein <nick@iMac.local>
2017-02-13 15:28:57 +0100

    heroku

--- server.js
+++ server.js
@@ -43,20 +43,20 @@ hbs.registerHelper('screamIt', (text) => {
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcome: 'Oi, whats the crack?',
+    welcome: 'Oi, whats the crack?'
   });
 });
 
-app.get('/', (req, res) => {
+app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
     pageTitle: 'Projects',
-    projectsWelcome: 'My personal projects',
+    projectsWelcome: 'My personal projects'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
   });
 });
 

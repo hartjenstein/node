c9d299fb7f9e32117230673bb9c8be6728dae992
nicolas hartjenstein <hartjenstein@Hartjensteins.local>
2017-02-13 10:00:56 +0100

    @home

--- node-servers/server.js
+++ node-servers/server.js
@@ -1,33 +1,65 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require ('fs');
 
-var app = express();
+const port = process.env.PORT || 3000;
+let app = express();
 
-app.set('viewengine', 'hbs');
-app.use(express.static(__dirname + "/public"));
-// handler for an http get request
+// partials are used as templates for site wide recurring html
+hbs.registerPartials(__dirname + '/views/partials');
+
+// setting up our express middleware together with handlebars for templating
+app.set('view engine', 'hbs');
+
+
+//middleware is used to configure express - adds on features
+// app.use is how you register middleware
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = (`${now} : ${req.method} ${req.url}`);
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to write log');
+    }
+  });
+  next();
+});
+/*app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});*/
+
+app.use(express.static(__dirname + '/public'));
+
+// helper functions - can be embeded in template - in this case in the footer
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-   // res.send('<h1>Hello Express</h1>');
-   res.send({
-       name: 'Norbert',
-       likes: ['you know what', 'my favorite records']
-   });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcome: 'Oi, whats the crack?',
+  });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs', {
-        pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
-    });
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
 });
-     
 
-
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
-    res.send({
-    errorMessage: 'Bad Connection!'
-    });
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
 
-// binds the application to a port on our machine
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});

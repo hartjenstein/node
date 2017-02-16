2227a4a485d890decd13ec60d8cd48947f1006d9
Nick Hartjenstein <nick@iMac.local>
2017-02-16 10:41:53 +0100

    @kurs mongo delete

--- mongoDB/node-todo-api/playground/mongodb-delete.js
+++ mongoDB/node-todo-api/playground/mongodb-delete.js
@@ -1,6 +1,6 @@
 // const MongoClient = require('mongodb').MongoClient;
 // no need for the .MongoClient at the end when we're using object destructuring
-const { MongoClient, ObjectID } = require('mongodb');
+const { MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/todoApp', (err, db) =>  {
     if(err) {
@@ -9,10 +9,12 @@ MongoClient.connect('mongodb://localhost:27017/todoApp', (err, db) =>  {
     console.log('connected to mongo');
 
 // deleteMany
-
+db.collection('Users').findOneAndDelete({"_id": new ObjectID("58a44cc535cfdade8ca94225")}).then((result)=> {
+    console.log(JSON.stringify(result, undefined, 2));
+});
 // deleteOne
 
 // findOneAndDelete
 
-// db.close();
+// db.close();  
 });

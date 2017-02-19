cdeb943b8237b898aaed4c7bfaf05c42b14dbd7d
nicolas hartjenstein <hartjenstein@Hartjensteins.local>
2017-02-19 22:49:25 +0100

    added route testing

--- mongoDB/node-todo-api/server/tests/server.test.js
+++ mongoDB/node-todo-api/server/tests/server.test.js
@@ -1,11 +1,12 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongoDB');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
-    text: 'First test todo'
+    _id: new ObjectID(),
+     text: 'First test todo'
 }, {
     text: 'Second test todo'
 }];
@@ -62,6 +63,32 @@ describe('GET /todos', () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+// ----- _id.toHexString() converts obejct id to string
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+       request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done)
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get(`/todos/string`)
+            .expect(404)
             .end(done);
     });
 });
